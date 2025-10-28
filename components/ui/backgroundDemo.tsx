276d2c0e2b869cfbfd36bd29954356c714782c9a
ASTROFO7 <petkovandrey03@gmail.com>
2025-10-28 09:36:43 +0100

    changed the profile image

--- components/ui/backgroundDemo.tsx
+++ components/ui/backgroundDemo.tsx
@@ -4,13 +4,14 @@ import { IconAppWindow } from "@tabler/icons-react";
 import Image from "next/image";
 import { BackgroundGradient } from "./Background-gradient";
 import exampleImg from '../../public/images/profilePicture2.jpg';
+import profileImg from '../../public/images/profileImage.jpeg';
 
 export function BackgroundGradientDemo() {
   return (
     <div>
       <BackgroundGradient className="rounded-full max-w-sm p-0.5 bg-transparent dark:bg-transparent m-b ">
         <Image
-          src={exampleImg}
+          src={profileImg}
           alt="jordans"
           height="400"
           width="400"
