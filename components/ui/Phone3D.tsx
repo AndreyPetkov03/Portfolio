5772002116ac5c4f5e4d984a1fd8f61d6abde9d5
ASTROFO7 <petkovandrey03@gmail.com>
2025-10-28 10:06:04 +0100

    updated the spline scene link

--- components/ui/Phone3D.tsx
+++ components/ui/Phone3D.tsx
@@ -4,7 +4,7 @@ export default function Phone3D() {
   return (
     <main className="w-full h-[37.5rem] md:h-full hover:cursor-pointer rounded-xl relative overflow-hidden bg-[#0C0E23] border border-[#1c1c3f]">
       <Spline
-        scene="https://prod.spline.design/BOxXyGTUti294mnY/scene.splinecode" 
+        scene="https://prod.spline.design/bvW-rGMs2tMnuqxh/scene.splinecode" 
       />
     </main>
   );
