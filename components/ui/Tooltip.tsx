4e7747b3967be499c12ed68b8e4758143c03487f
ASTROFO7 <petkovandrey03@gmail.com>
2025-10-31 08:53:25 +0100

    added aditional tooltops and new svg

--- components/ui/Tooltip.tsx
+++ components/ui/Tooltip.tsx
@@ -12,12 +12,16 @@ const TOOLTIP_SETS = {
     { id: 3, name: "", designation: "Next.js", image: "/projectIcons/nextProject.svg" },
     { id: 4, name: "", designation: "TypeScript", image: "/projectIcons/typescriptProject.svg" },
     { id: 5, name: "", designation: "Framer Motion", image: "/projectIcons/framermotionProject.svg" },
+    { id: 6, name: "", designation: "Spline", image: "/projectIcons/splineProject.svg" },
   ],
   intranet: [
     { id: 1, name: "", designation: "React", image: "/projectIcons/reactProject.svg" },
     { id: 2, name: "", designation: "Tailwind", image: "/projectIcons/tailwindProject.svg" },
     { id: 3, name: "", designation: "Next.js", image: "/projectIcons/nextProject.svg" },
     { id: 4, name: "", designation: "TypeScript", image: "/projectIcons/typescriptProject.svg" },
+    { id: 5, name: "", designation: "Figma", image: "/projectIcons/figmaProject.svg" },
+    { id: 6, name: "", designation: "Docker", image: "/projectIcons/dockerProject.svg" },
+    { id: 7, name: "", designation: "Sql", image: "/projectIcons/sqlProject.svg" },
     // Add more specific logos for intranet project
   ],
   gallery: [
@@ -25,6 +29,7 @@ const TOOLTIP_SETS = {
     { id: 2, name: "", designation: "Tailwind", image: "/projectIcons/tailwindProject.svg" },
     { id: 3, name: "", designation: "Next.js", image: "/projectIcons/nextProject.svg" },
     { id: 4, name: "", designation: "TypeScript", image: "/projectIcons/typescriptProject.svg" },
+    { id: 5, name: "", designation: "Supabase", image: "/projectIcons/supabaseProject.svg" },
     // Add more specific logos for gallery project
   ],
   comingSoon: Array.from({ length: 5 }, (_, i) => ({
