d036120f7abca823d0633d537b693d2163d23a6c
ASTROFO7 <petkovandrey03@gmail.com>
2025-11-03 11:53:59 +0100

    made icons even bigger

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -8,32 +8,32 @@ export default function Footer() {
         <p className="ml-0 mb-3">Copyright ©{currentYear} Andrey Petkov</p>
         <div className="flex ml-0 items-center">
             <a href="https://www.linkedin.com/in/andrey-petkov-5a1040373" target="_blank" rel="noopener noreferrer">
-                <div className="cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-100 rounded-lg border border-white/[0.2] p-1.5 mr-2 w-8 h-8">
+                <div className="cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-100 rounded-lg border border-white/[0.2] p-2 mr-2 w-9 h-9">
                     <Image 
                         src="/LOGOS/linkedin.svg" 
                         alt="linkedin" 
-                        width={18}
-                        height={18}
+                        width={20}
+                        height={20}
                     />
                 </div>
             </a>
             <a href="mailto:petkovandrey03@gmail.com" target="_blank" rel="noopener noreferrer">
-                <div className="cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-100 rounded-lg border border-white/[0.2] p-1.5 mr-2 w-8 h-8">
+                <div className="cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-100 rounded-lg border border-white/[0.2] p-2 mr-2 w-9 h-9">
                     <Image 
                         src="/LOGOS/emailSvg.svg" 
                         alt="mail" 
-                        width={18}
-                        height={18}
+                        width={20}
+                        height={20}
                     />
                 </div>
             </a>
             <a href="https://github.com/AndreyPetkov03" target="_blank" rel="noopener noreferrer">
-                <div className="cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-100 rounded-lg border border-white/[0.2] p-1.5 mr-2 w-8 h-8">
+                <div className="cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-100 rounded-lg border border-white/[0.2] p-2 mr-2 w-9 h-9">
                     <Image 
                         src="/LOGOS/github.svg" 
                         alt="github" 
-                        width={18}
-                        height={18}
+                        width={20}
+                        height={20}
                     />
                 </div>
             </a>
