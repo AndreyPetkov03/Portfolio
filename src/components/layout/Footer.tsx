d63e8e67944411c1feea6702b167606cbfd91dbf
ASTROFO7 <petkovandrey03@gmail.com>
2025-11-04 08:34:22 +0100

    added hover effects to footer contact me buttons

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
         <p className="ml-0 mb-3">Copyright ©{currentYear} Andrey Petkov</p>
         <div className="flex ml-0 items-center">
             <a href="https://www.linkedin.com/in/andrey-petkov-5a1040373" target="_blank" rel="noopener noreferrer">
-                <div className="cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-100 rounded-lg border border-white/[0.2] p-2 mr-2 w-9 h-9">
+                <div className="cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-100 rounded-lg border border-white/[0.2] p-2 mr-2 w-9 h-9 transition-all duration-300 hover:scale-110 hover:bg-opacity-100 hover:border-white/[0.4] hover:shadow-lg hover:shadow-white/[0.2]">
                     <Image 
                         src="/LOGOS/linkedin.svg" 
                         alt="linkedin" 
@@ -18,7 +18,7 @@ export default function Footer() {
                 </div>
             </a>
             <a href="mailto:petkovandrey03@gmail.com" target="_blank" rel="noopener noreferrer">
-                <div className="cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-100 rounded-lg border border-white/[0.2] p-2 mr-2 w-9 h-9">
+                <div className="cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-100 rounded-lg border border-white/[0.2] p-2 mr-2 w-9 h-9 transition-all duration-300 hover:scale-110 hover:bg-opacity-100 hover:border-white/[0.4] hover:shadow-lg hover:shadow-white/[0.2]">
                     <Image 
                         src="/LOGOS/emailSvg.svg" 
                         alt="mail" 
@@ -28,7 +28,7 @@ export default function Footer() {
                 </div>
             </a>
             <a href="https://github.com/AndreyPetkov03" target="_blank" rel="noopener noreferrer">
-                <div className="cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-100 rounded-lg border border-white/[0.2] p-2 mr-2 w-9 h-9">
+                <div className="cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-100 rounded-lg border border-white/[0.2] p-2 mr-2 w-9 h-9 transition-all duration-300 hover:scale-110 hover:bg-opacity-100 hover:border-white/[0.4] hover:shadow-lg hover:shadow-white/[0.2]">
                     <Image 
                         src="/LOGOS/github.svg" 
                         alt="github" 
